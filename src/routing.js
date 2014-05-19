3a373ea3926c7b105ddc88b3114af9e60ee5f667
Andres Ledesma <landreus@gmail.com>
2014-05-19 17:04:33 +0300

    Reestructured the files.

--- src/routing.js
+++
@@ -1,5 +0,0 @@
-var routes = [
-    {id: 'awqvjpxou016pabc', hex: new BigInteger("a8fb534a38cdaf70760291cfe6dcb7eaaa1d895b", 16)},
-    {id: 'bwqvypuou112pafu', hex: new BigInteger("cbebd0b7789913bf5f9e1c38f6b0c81a7462f9e4", 16)},
-    {id: 'trevepuou192p8fx', hex: new BigInteger("407611b8068c8d29a0ca9ff59e01eafdbf716bd5", 16)}
-];
